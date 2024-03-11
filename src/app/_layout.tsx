332bf850b7a1204dffc351e9bc35ea514c931df4
Grigory Yakovlev <71254983+ykvlv@users.noreply.github.com>
2024-03-11 18:02:43 +0300

    default components removed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,8 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-
-import { useColorScheme } from "@/components/useColorScheme";
 
 import "../../global.css";
 
@@ -51,14 +44,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{}} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{}} />
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+    </Stack>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,41 +1,27 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link } from "expo-router";
 import React from "react";
-import { Pressable, ScrollView, StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
-import { useColorScheme } from "@/components/useColorScheme";
-import Colors from "@/constants/Colors";
+import { Pressable, ScrollView, StyleSheet, View, Text } from "react-native";
 
 export default function TabOneScreen() {
-  const colorScheme = useColorScheme();
-
   return (
     <ScrollView>
       <View style={styles.container}>
         <View style={styles.rowContainer}>
           <Text style={styles.title}>Tab One</Text>
-          <View />
           <Link href="/modal" asChild>
             <Pressable>
               {({ pressed }) => (
                 <FontAwesome
                   name="info-circle"
                   size={25}
-                  color={Colors[colorScheme ?? "light"].text}
                   style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                 />
               )}
             </Pressable>
           </Link>
         </View>
-        <View
-          style={styles.separator}
-          lightColor="#eee"
-          darkColor="rgba(255,255,255,0.1)"
-        />
-        <EditScreenInfo path="app/(tabs)/index.tsx" />
+        <View style={styles.separator} />
       </View>
     </ScrollView>
   );
@@ -58,7 +44,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
     width: "80%",
   },
